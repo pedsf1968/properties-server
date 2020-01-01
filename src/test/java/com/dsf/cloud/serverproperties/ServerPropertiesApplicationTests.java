9252352127c63f6c10d8328b9d84bdd651635947
PEDSF <pe.dsf@free.fr>
2020-01-01 17:51:45 +0100

    Server properties spring cloud

---
+++ src/test/java/com/dsf/cloud/serverproperties/ServerPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsf.cloud.serverproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
